ed2de1ad55d87193a0fae427717419ece98d822c
unknown <tanvirsiam1101@gmail.com>
2023-06-05 12:48:35 +0600

    webhook

---
+++ pages/api/bKash/production.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+const superagent = require("superagent");
+
+// type Data = {
+//   name: string
+// }
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+//   const myHeaders = new Headers();
+
+//   const raw = JSON.stringify({
+//     user_id: localStorage.getItem("user_id"),
+// });
+
+//   const requestOptions = {
+//     method: "POST",
+//     headers: myHeaders,
+//     body: raw,
+//   };
+//   const url = 'https://api.kabbik.com/v3/bkash/bKash/auth';
+  
+//   try {
+//     const fetchResponse = await fetch(url, requestOptions);
+
+//     const data = await fetchResponse.json();
+//     data.status = fetchResponse.status;
+//     return data;
+//   } catch (e) {
+//     return e;
+//   }
+
+
+  var requestMethod = req.method;
+  const body = JSON.stringify(req.body);
+  if (requestMethod == 'POST') {
+    superagent.post('https://api.kabbik.com/v3/bkash/webhookProduction')
+      .send(req.body)
+      .set('Accept', 'application/json')
+      .set('headers', JSON.stringify(req.headers))
+      .then((respon: { body: any; }) => {
+        res.status(200).json(respon.body);
+      });
+  }
+  else {
+    res.status(200).json({ errorMessage: 'Wrong Responese!' });
+  }
+}
